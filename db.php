9dba4435084ee1373649de9f2969618382012333
Prometheus <151749993+plutonium00001@users.noreply.github.com>
2023-11-24 11:12:02 +0100

    Update db.php
    Modification du fichier db.php 
    
    Remplacement des valeurs présentes par des indications "facilement compréhensible" pour les utilisateurs

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
 
-$pdo = new PDO('mysql:dbname=espace_membre_v3;host=localhost','root','root');
+$pdo = new PDO('mysql:dbname='nom_de_votre_base';host='adresse_du_serveur','nom_utilisateur','mot_de_passe');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
